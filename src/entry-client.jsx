09630d8e5e27fbc84b5ea1523f4f22730d018c6f
manu2699 <maneeshvijaykar@gmail.com>
2025-05-20 15:43:07 +0530

    client & index html fixes

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,11 +1,12 @@
+import { hydrateRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import App from './App';
+
 import './index.css'
-import { StrictMode } from 'react'
-import { hydrateRoot } from 'react-dom/client'
-import App from './App'
 
 hydrateRoot(
   document.getElementById('root'),
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
